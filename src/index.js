a8b55cf189f41b95ece34b295994cdc9edb4c6bf
SoleneBzght <solene.bouzag@gmail.com>
2021-12-29 19:51:55 +0100

    Added JS - weather details section + added HTML/CSS date info

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temp");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].main;
+  let humidityElement = document.querySelector("#humidityResult");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#windResult");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "df6a343d5addb6386bf26325b01ba697";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${apiKey}&units=metric`;
+
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);

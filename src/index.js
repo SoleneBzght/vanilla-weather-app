b47e75df7a1b9ab8d296e57c607571ff6203156f
SoleneBzght <solene.bouzag@gmail.com>
2021-12-29 23:53:22 +0100

    updated JS & HTML & CSS - added units conversion

--- src/index.js
+++ src/index.js
@@ -44,22 +44,47 @@ function displayTemperature(response) {
   let iconElement = document.querySelector("#main-icon");
   iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].main);
-}
 
+  celsiusTemp = response.data.main.temp;
+}
 function search(city) {
   let apiKey = "df6a343d5addb6386bf26325b01ba697";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
-
 function handleSubmit(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
   search(searchInputElement.value);
 }
 
+function displayFahrenheitTemp(event) {
+  event.preventDefault();
+  let tempElement = document.querySelector("#temp");
+  celsiusLink.classList.replace("celsiusDefault", "celsiusNew");
+  fahrenheitLink.classList.replace("fahDefault", "fahNew");
+  let fahrenheitTemp = (celsiusTemp * 9) / 5 + 32;
+  tempElement.innerHTML = Math.round(fahrenheitTemp);
+}
+function displayCelsiusTemp(event) {
+  event.preventDefault();
+  fahrenheitLink.classList.replace("fahNew", "fahDefault");
+  celsiusLink.classList.replace("celsiusNew", "celsiusDefault");
+  let tempElement = document.querySelector("#temp");
+  tempElement.innerHTML = Math.round(celsiusTemp);
+}
+
+let celsiusTemp = null;
+
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemp);
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemp);
+
+search("Paris");

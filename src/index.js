20e9106163dbc88b1f14b5a04d654854d7c59b01
SoleneBzght <solene.bouzag@gmail.com>
2022-01-26 23:29:03 +0100

    API key V2

--- src/index.js
+++ src/index.js
@@ -58,7 +58,7 @@ function displayForecast(response) {
   forecastElement.innerHTML = forecastHTML;
 }
 function getForecast(coordinates) {
-  let apiKey = "7ea8bed40f07c47d46157464ee87125f";
+  let apiKey = "fe1483f743b581b5520a1b725af03a49";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
@@ -87,7 +87,7 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-  let apiKey = "7ea8bed40f07c47d46157464ee87125f";
+  let apiKey = "fe1483f743b581b5520a1b725af03a49";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
